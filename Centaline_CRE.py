3ad436765dc5b478d72962191db8fecb7ffa16f6
HKMAEC1 <138672639+HKMAEC1@users.noreply.github.com>
2024-05-09 11:14:04 +0800

    Update Centaline_CRE.py

--- Centaline_CRE.py
+++ Centaline_CRE.py
@@ -1,19 +1,19 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
-from datetime import datetime, time as dt_time
-import time as sys_time
+from datetime import datetime
+import time
 
-#set webdriver
+# Set webdriver options
 chrome_options = webdriver.chrome.options.Options()
 
 prefs = {
-'download.extensions_to_open': 'xml',
-'safebrowsing.enabled': True
+    'download.extensions_to_open': 'xml',
+    'safebrowsing.enabled': True
 }
-## Uncomment the below to let the program to run in background
+## Uncomment the below to let the program run in the background
 options = webdriver.ChromeOptions()
-options.add_experimental_option('prefs',prefs)
+options.add_experimental_option('prefs', prefs)
 options.add_argument("start-maximized")
 options.add_argument("--headless")
 # options.add_argument("disable-infobars")
@@ -24,49 +24,46 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
 def scrape_data():
-    # Retail
-    # Lease
-    driver = webdriver.Chrome('/usr/bin/chromedriver', options = options)
+    # Retail Lease
+    driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
     url = 'https://oir.centanet.com/lease/retail/'
     driver.get(url)
     retail_lease = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/div[1]/div[4]/div/div/div[1]/h1/span').text
-    
-    # Sales
-    driver = webdriver.Chrome('/usr/bin/chromedriver', options = options)
+
+    # Retail Sales
+    driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
     url = 'https://oir.centanet.com/sale/retail/'
     driver.get(url)
     retail_sales = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/div[1]/div[4]/div/div/div[1]/h1/span').text
-    
-    # Office
-    # Lease
-    driver = webdriver.Chrome('/usr/bin/chromedriver', options = options)
+
+    # Office Lease
+    driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
     url = 'https://oir.centanet.com/lease/office/'
     driver.get(url)
     office_lease = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/div[1]/div[4]/div/div/div[1]/h1/span').text
-    
-    # Sales
-    driver = webdriver.Chrome('/usr/bin/chromedriver', options = options)
+
+    # Office Sales
+    driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
     url = 'https://oir.centanet.com/sale/office/'
     driver.get(url)
     office_sales = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/div[1]/div[4]/div/div/div[1]/h1/span').text
-    
-    # Factory
-    # Lease
-    driver = webdriver.Chrome('/usr/bin/chromedriver', options = options)
+
+    # Factory Lease
+    driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
     url = 'https://oir.centanet.com/lease/industrial/'
     driver.get(url)
     factory_lease = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/div[1]/div[4]/div/div/div[1]/h1/span').text
-    
-    # Sales
-    driver = webdriver.Chrome('/usr/bin/chromedriver', options = options)
+
+    # Factory Sales
+    driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
     url = 'https://oir.centanet.com/sale/industrial/'
     driver.get(url)
     factory_sales = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/div[1]/div[4]/div/div/div[1]/h1/span').text
-    
+
     d = datetime.now()
-    
+
     driver.quit()  # Quit the webdriver to release resources
-    
+
     return [d, retail_sales, retail_lease, office_sales, office_lease, factory_sales, factory_lease]
 
 # Scrape data
@@ -76,10 +73,10 @@ data = scrape_data()
 try:
     df = pd.read_excel("CRE.xlsx")
 except FileNotFoundError:
-    df = pd.DataFrame(columns=["Date", "Retail Sales", "Retail lease", "Office Sales", "Office lease", "Industrial Sales", "Industrial lease"])
+    df = pd.DataFrame(columns=["Date", "Retail Sales", "Retail Lease", "Office Sales", "Office Lease", "Industrial Sales", "Industrial Lease"])
 
 # Append new data to the dataframe
-df = df.append(pd.Series(data, index=df.columns), ignore_index=True)
+df = pd.concat([df, pd.Series(data, index=df.columns)], ignore_index=True)
 
 # Save dataframe to file
 df.to_excel("CRE.xlsx", sheet_name="sheet1", index=False)
